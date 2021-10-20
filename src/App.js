88320a62ee6dd525d6c618a8ad5baf6688f8f44d
Gaurish Prakhar Saini <gaurishprakhar@gmail.com>
2021-10-20 05:51:00 +0530

    intial components added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
+import Search from "./components/Search";
+import AddAppoitnment from "./components/AddAppointment"
+
 function App() {
   return (
     <div className="App container mx-auto mt-3 font-thin">
-      <h1 className="text-5xl">
-        Your Appointments
-      </h1>
+      <h1 className="text-5xl mb-3">Your Appointments</h1>
+      <AddAppoitnment />
+      <Search />
     </div>
   );
 }
