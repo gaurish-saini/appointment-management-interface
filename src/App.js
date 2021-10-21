f17f8df90b186ef54a4d1d58f4e38de34e4c1d5e
Gaurish Prakhar Saini <gaurishprakhar@gmail.com>
2021-10-22 04:29:47 +0530

    added data.json

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import Search from "./components/Search";
-import AddAppoitnment from "./components/AddAppointment"
+import AddAppoitnment from "./components/AddAppointment";
+import AppointmentInfo from "./components/AppointmentInfo";
+import appointmentList from "./data.json";
 
 function App() {
   return (
@@ -7,6 +9,11 @@ function App() {
       <h1 className="text-5xl mb-3">Your Appointments</h1>
       <AddAppoitnment />
       <Search />
+      <ul className="divide-y divide-gray-200">
+        {appointmentList.map((appointment) => (
+          <AppointmentInfo key={appointment.id} appointment={appointment} />
+        ))}
+      </ul>
     </div>
   );
 }
